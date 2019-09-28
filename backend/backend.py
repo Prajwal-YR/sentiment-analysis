a7e501791f04beb8cf58e264f33046ebdc363f7d
aThinkingNeal <31988374+AthinkingNeal@users.noreply.github.com>
2019-09-29 02:04:57 +0530

    Create backend.py

---
+++ backend/backend.py
@@ -0,0 +1,23 @@
+import pandas as pd
+twitter_username = ['noneprivacy','iamsrk']
+def scrapeTwitter(username,limit):
+    import sys
+    sys.path.append("/usr/local/lib/python3.7/site-packages")
+    import twint
+    # notice the package path problem, I use the above commands to add the twint package into my current path
+    
+    c = twint.Config()
+    c.Username = username
+    c.Output = username + ".csv"
+    c.Store_csv = True
+    c.Limit = limit
+    twint.run.Search(c)
+
+
+
+for user_name in twitter_username:
+    print(type(user_name))
+    scrapeTwitter(user_name, 10)
+    userDf = pd.read_csv(user_name + ".csv")
+    student_twitter = userDf['tweet'].tolist()
+    print(student_twitter)

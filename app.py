d2493450b59ce794db3ce2a739259b959632577d
simritaj <42865187+simritaj@users.noreply.github.com>
2019-09-29 03:30:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for,request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
@@ -15,7 +15,8 @@ db=firestore.client()
 #    docs=db.collection(u'students').stream()
 #    for doc in docs:
 #        for 
-#database_dict = {studentname: Sim, attendance:}
+database_dict = {1: [1033,"simrita",98,"A","B",21,"y"], 2:[1097,"john",89,"B","C",23,"n"]}
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissupposedtobesecret'
@@ -26,8 +27,33 @@ class SignUpForm(FlaskForm):
     password = PasswordField('Password', validators = [InputRequired(), Length(min=8, max = 20)])
     remember = BooleanField('RememberMe!')
 
-@app.route('/')
+@app.route('/index.html', methods=["POST","GET"])
 def index():
+    if request.method == "GET":
+        return render_template('index.html', database_dict = database_dict)
+    else:
+        grade_file = request.files['grades']
+        attendance_file = request.files['attendance']
+    return render_template('index.html', database_dict = database_dict,msg="success")
+
+@app.route('/login.html' ,methods=["POST","GET"])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        return redirect(url_for("index"))
+    return render_template('login.html')
+
+@app.route('/register.html' ,methods=["POST","GET"])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        name = request.form['name']
+        password = request.form['password']
+        return redirect(url_for("login"))
     return render_template('index.html')
 
 @app.route('/signup', methods = ['GET','POST'])

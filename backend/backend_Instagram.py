ee0fdfbd615a5a1a3b2eb1d400a000d478eecaed
aThinkingNeal <31988374+AthinkingNeal@users.noreply.github.com>
2019-09-29 14:19:59 +0530

    return the path of folder
    take a list of user names of Instagram, return a list of the path of folders

--- backend/backend_Instagram.py
+++ backend/backend_Instagram.py
@@ -1,10 +1,19 @@
+
 # pip install instagram-scraper
 import os
-Instagram_username = ['athinkingneal']
+Instagram_username = ['athinkingneal','_peebz_']
+
+path_list = []
+
 def scrapeInstagram(username):
-	os.system("instagram-scraper username -m 40")
+	os.system("instagram-scraper " + username + " -m 40 --proxies true")
 	# scrape maximum 40 pictures
+	current_path = os.getcwd()
+	image_folder = os.path.join(current_path,username)
+	path_list.append(image_folder)
 
 for username in Instagram_username:
 	scrapeInstagram(username)
 	# the images are stored into <current working directory>/<username>
+
+#print(path_list)

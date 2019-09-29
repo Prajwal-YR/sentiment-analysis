00da72b7b2d949c932e7814c2884b20a8ddfcb0d
Prajwal-YR <55399259+Prajwal-YR@users.noreply.github.com>
2019-09-29 08:06:42 +0530

    Update firestore_api.py

--- firestore_api.py
+++ firestore_api.py
@@ -15,14 +15,14 @@ def add_user(name,user,psw):
     print(next_id)
     json={
         "name":name,
-        "username":user,
+        "email":user,
         "password":psw
     }
     doc_ref=db.collection('administrator').document(str(next_id)).set(json)
     return
 		
-def check_user(name,psw):
-    doc_ref=db.collection('administrator').where(u'username',u'==',name).stream()
+def check_user(email,psw):
+    doc_ref=db.collection('administrator').where(u'username',u'==',email).stream()
     #if len(list(doc_ref)) == 0:
         #print("Empty")
         #user DNE
@@ -55,19 +55,41 @@ def update_attendance(file):
         doc_ref.update({u'attendance': attendance})
     return
 
-def display_table():
-    docs_ref=db.collection('students').stream()
+def display_table(email):
+    docs_ref = db.collection('administrator').where('email','==','email').stream()
     dictionary={}
     no_of_dep=0
     for doc in docs_ref:
         dic=doc.to_dict()
         id=doc.id
-        print(id)
+        sid=dic['students']
+        
         tweet=db.collection('tweets').where('student_id','==',str(id)).stream()
         for t in tweet:
             no_of_dep=len(t.to_dict()['tweets'])
-            print("Count = ",no_of_dep)
          
-        dictionary[id]=dic['name'],dic['attendance'],dic['grades'][-1],dic['predicted_grade'],no_of_dep
+        dictionary[id]=id,dic['name'],dic['attendance'],dic['grades'][-1],dic['predicted_grade'],no_of_dep
     return dictionary
-		
+
+def student_details(sid):
+    docs_ref=db.collection('tweets').where('student_id','==',str(sid)).stream()
+    stud=db.collection('students').document(str(sid)).get()
+    dictionary={}
+    sname=None
+    tlist=[]
+    times=[]
+    sname=stud.to_dict()['name']
+    for doc in docs_ref:
+        twitter_username=doc.id
+        dic=doc.to_dict()
+        no_of_dep=len(dic['tweets'])
+        for i in dic['tweets']:
+            tlist.append(i['text'])
+            times.append(i['timestamp'])
+    return sname,tlist,times
+
+def add_student(form):
+    did=form.pop('id',None)
+    doc_ref=db.collection('students').document(str(did)).set(form)
+    tweet_ref=db.collection('tweets').document(str(form['twitter'])).set({'student_id':str(did)})
+
